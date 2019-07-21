519210f43c64e245b638db1000b93405d3bb4098
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-21 14:21:41 +0200

    example of how to show hardcoded dynamic content

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
 <?php
 
-// PHP comments are not visible in page source
-// Create an array with a list of articles
-$articles = [ "First Post", "Another Post", "Read This!" ]; /* example of multiple lined comment */
+$articles = [
+    [
+        "title" => "It was all a dream!",
+        "content" => "I used to read Word Up! magazine."
+    ],
+    [
+        "title" => "Birthdays was the worst days.",
+        "content" => "Now we sip champagne when we thirsty!"
+    ],
+    [
+        "title" => "You know very well who you are!",
+        "content" => "Don't let them hold you down."
+    ]
+];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Blog</title>
+    <meta charset="utf-8">
+</head>
+<body>
 
-foreach ($articles as $article) {
-    echo $article, ", ";
-}
+    <header>
+        <h1>My Blog</h1>
+    </header>
+
+    <main>
+        <ul>
+            <?php foreach ($articles as $article): ?>
+                <li>
+                    <article>
+                        <h2><?= $article['title']; ?></h2>
+                        <p><?= $article['content']; ?></p>
+                    </article>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </main>
+</body>
+</html>
