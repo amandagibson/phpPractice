c6491036ce9c1b874bc43c6aa431c7c788c06138
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-21 12:28:46 +0200

    initial commit of misc practice work

---
+++ hello.php
@@ -0,0 +1,82 @@
+<?php
+
+// $articles = [
+//   "a" => "First post",
+//   "b" => "Second post",
+//   "c" => "Third post"
+// ];
+
+// foreach ($articles as $index => $article) {
+//   echo $index . " = " . $article, ", ";
+// }
+
+// IF ELSE
+// $articles = [ "Monday", "Tuesday" ];
+
+// if (empty($articles)) {
+//   echo "The array is empty.";
+// } else {
+//   echo "The array is not empty.";
+// }
+
+// WHILE LOOP
+// $month = 1;
+
+// while ($month <= 12) {
+//     echo $month . ", ";
+//     $month = $month + 1;
+// }
+
+// FOR LOOP
+// for (init; test; change) {
+//   code to run while test is true
+// }
+
+// for ($i = 1; $i <= 10; $i++) {
+//     echo $i  . ", ";
+// }
+
+// ELSEIF
+// $hour = 23;
+
+// if ($hour < 12) {
+//     echo "Good Morning!";
+// } elseif ($hour < 18) {
+//     echo "Good Afternoon!";
+// } elseif ($hour < 22) {
+//     echo "Good Evening.";
+// } else {
+//     echo "Goodnight.";
+// }
+
+//$array = [];
+
+// for ($i = 1; $i <= 10; $i++) {
+
+//   if ($i < 4) {
+//       $array[$i] = "a";
+//   } elseif ($i > 3 and $i < 8) {
+//       $array[$i] = "b";
+//   } else {
+//       $array[$i] = "c";
+//   }
+
+// }
+
+
+// $day = "Fri";
+
+// switch ($day) {
+//     case "Mon":
+//         echo "Monday";
+//         break;
+//     case  "Tue":
+//         echo "Tuesday";
+//         break;
+//     case "Wed":
+//         echo "Wednesday";
+//         break;
+//     default:
+//         echo "No info available for that day";
+//         break;
+// }
