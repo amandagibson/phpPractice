d95cece627fa496b9186c1e07eec29c7e46a3164
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-24 10:42:39 +0200

    adds an article page that queries from the database. adds working links to article titles.

--- article.php
+++ article.php
@@ -16,14 +16,14 @@ echo "Connected successfully.";
 
 $sql = "SELECT *
         FROM article
-        ORDER BY published_at;";
+        WHERE id = " . $_GET['id'];
 
 $results = mysqli_query($conn, $sql);
 
 if ($results === false) {
     echo mysqli_error($conn);
 } else {
-    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    $article = mysqli_fetch_assoc($results);
 }
 
 ?>
@@ -40,21 +40,13 @@ if ($results === false) {
     </header>
 
     <main>
-        <?php if (empty($articles)): ?>
-            <p>No articles found.</p>
+        <?php if ($article === null): ?>
+            <p>Article not found.</p>
         <?php else: ?>
-
-        <ul>
-            <?php foreach ($articles as $article): ?>
-                <li>
-                    <article>
-                        <h2><a href="article.php?id=<?= $article['id']; ?>"><?= $article['title']; ?></a></h2>
-                        <p><?= $article['content']; ?></p>
-                    </article>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-
+            <article>
+                <h2><?= $article['title']; ?></h2>
+                <p><?= $article['content']; ?></p>
+            </article>
         <?php endif; ?>
     </main>
 </body>
